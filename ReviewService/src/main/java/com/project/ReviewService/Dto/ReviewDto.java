2609b629c61abf1959d4aecf5ca9f0d5f68f1780
MICHAEL KINFE <akikinfe@gmail.com>
2023-01-25 21:54:34 -0500

    in ReviewService sending the message with kafka

--- ReviewService/src/main/java/com/project/ReviewService/Dto/ReviewDto.java
+++ ReviewService/src/main/java/com/project/ReviewService/Dto/ReviewDto.java
@@ -1,10 +1,19 @@
 package com.project.ReviewService.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReviewDto {
 
  private String description;
  private String isbn;
  private Integer rating;
- private String customerNumber;
+ private long customerNumber;
 
 }
--- ReviewService/src/main/java/com/project/ReviewService/controller/ReviewController.java
+++ ReviewService/src/main/java/com/project/ReviewService/controller/ReviewController.java
@@ -1,6 +1,8 @@
 package com.project.ReviewService.controller;
 
 import com.project.ReviewService.Dto.ReviewDto;
+import com.project.ReviewService.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,10 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/reviews")
 public class ReviewController {
+    @Autowired
+    ReviewService reviewService;
 
     @PostMapping
     public ResponseEntity<?> createReview(@RequestBody ReviewDto reviewDto){
-        return null;
+        return ResponseEntity.ok().body(reviewService.createReview(reviewDto));
     }
 
 
